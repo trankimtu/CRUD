41afe28111e8e29f3fa3d694cfe146e82131f295
Tu Tran <trankimtu@gmail.com>
2023-11-12 22:51:29 -0800

    Load product component to app module - Modify readme.md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductModule } from './product/product.module'; //5.2 - Load product component to app component (5.1 in product.modules.ts)
+import { ProductModule } from './product/product.module'; //6.2 - Load product component to app component (6.1 in product.modules.ts, 6.3 in app.component.html)
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import { ProductModule } from './product/product.module'; //5.2 - Load product c
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ProductModule //5.2 - Load product component to app component (5.1 in product.modules.ts, 5.3 in app.component.html)
+    ProductModule //6.2 - Load product component to app component (6.1 in product.modules.ts, 6.3 in app.component.html)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,6 +1,6 @@
 // Angular Import
-import { NgModule } from '@angular/core'; // 1- Generate when creating product module
-import { CommonModule } from '@angular/common'; // 1- Generate when creating product module
+import { NgModule } from '@angular/core'; // 2-- Generate when creating product module
+import { CommonModule } from '@angular/common'; // 2-- Generate when creating product module
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Using for p-dialog
 
@@ -9,21 +9,21 @@ import { DialogModule } from 'primeng/dialog';
 import { TableModule } from 'primeng/table';
 
 // Component Import
-import { ProductComponent } from './product.component'; // 2- Generate at creating product component
+import { ProductComponent } from './product.component'; // 3-- Generate at creating product component
 
 @NgModule({
   declarations: [
-    ProductComponent // 2- Generate at creating product component
+    ProductComponent // 3-- Generate at creating product component
   ],
   imports: [
     BrowserAnimationsModule,
     ButtonModule,
-    CommonModule, // 1- Generate when creating product module
+    CommonModule, // 2-- Generate when creating product module
     DialogModule,
-    HttpClientModule,
+    HttpClientModule, // 7.1-- Setup HTTP Service
     TableModule
   ],
-  exports: [ // 5.1 - Load product component to app component (5.2 in app.module.ts, 5.3 in app.component.html)
+  exports: [ // 6.1-- Load product component to app component (6.2 in app.module.ts, 6.3 in app.component.html)
     ProductComponent
   ]
 })
